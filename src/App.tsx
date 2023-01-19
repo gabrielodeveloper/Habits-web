fd9a5f6254bf37c79fff7f37ca504ed330bd9f06
gabrielodeveloper <gabriel0211oliveira15@gmail.com>
2023-01-19 04:43:17 -0300

    :tada: Habits: Inicial web project.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './styles/global.css';
+
+import { Habit } from './components/Habit';
+
+function App() {
+ 
+  return (
+    <div>
+      <Habit />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Habit/index.tsx
@@ -0,0 +1,7 @@
+
+
+export function Habit() {
+  return (
+    <h1>Habit</h1>
+  )
+}
