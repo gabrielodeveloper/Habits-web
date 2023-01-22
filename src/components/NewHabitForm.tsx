5d1a62a0d8c4a329cd3035f49e7e77b3ff0bf3f2
gabrielodeveloper <gabriel0211oliveira15@gmail.com>
2023-01-22 18:51:01 -0300

    Habits: connection with the api and registering new habits.

--- src/components/NewHabitForm.tsx
+++ src/components/NewHabitForm.tsx
@@ -1,23 +1,93 @@
 import { Check } from "phosphor-react";
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent, useState } from "react";
+import { api } from "../lib/axios";
+
+const availableWeekDays = [
+  'Domingo',
+  'Segunda-feira',
+  'Terça-feira',
+  'Quarta-feira',
+  'Quinta-feira',
+  'Sexta-feira',
+  'Sábado',
+]
 
 export function NewHabitForm() {
-  return (  
-    <form className="w-full flex flex-col mt-6">
+  const [title, setTitle ] = useState('');
+  const [weekDays, setWeekDays ] = useState<number[]>([]);
+
+  async function createNewHabit(event: FormEvent) {
+    event.preventDefault();
+
+    if(!title || weekDays.length === 0) {
+      return;
+    }
+
+    await api.post('habits', {
+      title,
+      weekDays,
+    });
+
+    alert('Hábito criado com sucesso.');
+    setTitle('');
+    setWeekDays([]);
+  }
+
+  function handleToggleWeekDay(weekDay: number) {
+    if(weekDays.includes(weekDay)) {
+      const weekDaysWithRomevedOne = weekDays.filter(day => day !== weekDay);
+
+      setWeekDays(weekDaysWithRomevedOne);
+    }else {
+      const weekDaysWithAddOne = [...weekDays, weekDay];
+
+      setWeekDays(weekDaysWithAddOne);
+    }
+  }
+
+  return (
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6">
       <label htmlFor="title" className="font-semibold leading-tight">
         Qual seu comprometimento?
       </label>
 
-      <input 
-        type="text" 
+      <input
+        type="text"
         id="title"
         placeholder="ex: Exercícios, dormir bem, etc..."
         className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
         autoFocus
+        onChange={event => setTitle(event.target.value)}
+        value={title}
       />
 
       <label htmlFor="" className="font-semibold leading-tight mt-4" >
         Qual a recorrência?
       </label>
+
+
+      <div className="mt-3 flex flex-col gap-3">
+        {availableWeekDays.map((weekDay, index) => (
+          <Checkbox.Root
+            key={weekDay} 
+            checked={weekDays.includes(index)}
+            className="flex items-center gap-2 group"
+            onCheckedChange={() => handleToggleWeekDay(index)}
+          >
+            <div className="w-8 h-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+              <Checkbox.Indicator>
+                <Check size={20} className="text-white" />
+              </Checkbox.Indicator>
+            </div>
+
+            <span className=" text-white leading-tight" >
+              {weekDay}
+            </span>
+          </Checkbox.Root>
+        ))}
+
+      </div>
 
       <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
         <Check size={20} weight="bold" />
